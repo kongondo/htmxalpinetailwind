0e5a1068aaa9e2be2bab4da53f06bde26bfca1ac
kongondo <kongondo@gmail.com>
2023-04-07 03:03:35 +0100

    Add tailwind config.js - in case we need it

---
+++ scripts/tailwind.config.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				clifford: "#da373d",
+			},
+		},
+	},
+	daisyui: {
+		themes: [
+			// "cupcake",
+			// "dark",
+			// "cmyk",
+			// {
+			// 	mytheme: {
+			// 		primary: "#8351ef",
+			// 		secondary: "#9586d8",
+			// 		accent: "#e88be0",
+			// 		neutral: "#161D27",
+			// 		"base-100": "#E5E5F0",
+			// 		info: "#99CEEB",
+			// 		success: "#1CA06E",
+			// 		warning: "#EFA906",
+			// 		error: "#F83069",
+			// 	},
+			// },
+		],
+	},
+	// plugins: [require("daisyui")],
+}
