db330598ada6afe060d7c7f8aecb4133ddea1c20
kongondo <kongondo@gmail.com>
2023-04-09 15:02:56 +0100

    Add demo content

--- product.php
+++ product.php
@@ -7,3 +7,16 @@ namespace ProcessWire;
 
 
 $breadcrumb = buildBreadCrumb($page);
+
+$imageMarkup = '<p>No product image</p>';
+if ($page->images->count()) {
+  $image = $page->images->first();
+  $thumb = $image->width(500);
+  $imageMarkup = "<img src='{$thumb->url}' alt='$image->description' class='px-4'>";
+}
+
+$content =
+  "<div class='grid gap-4 grid-cols-4'>" .
+  "<div class='col-span-full md:col-span-1'><p>{$imageMarkup}</p></div>" .
+  "<div class='md:col-span-3'>{$page->body}</div>" .
+  "</div>";
